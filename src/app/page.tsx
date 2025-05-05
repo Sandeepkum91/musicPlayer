499779a92ab2e031b47e86abbbe1ba97b11990d0
Sandeepkum91 <sandeepkumarsandeep180@gmail.com>
2025-05-05 16:10:53 +0530

    inital commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { SpotifySong } from "./spotify/page";
+
+
+export default function Home() {
+  return (
+    <div className="bg-amber-300">    
+    <SpotifySong/>
+    </div>
+  );
+}
